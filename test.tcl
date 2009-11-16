439d8b5cb2b820262f771c8f3b8aec8942c7d7e5
Chris Edwards <cedwards@sbis4079.otago.ac.nz>
2009-11-17 01:31:31 +1300

    Added basic test script.

---
+++ test.tcl
@@ -0,0 +1,21 @@
+#!/usr/bin/rlwrap tclsh8.5
+
+puts [load ./libtcljack.so]
+
+puts [jack]
+
+puts [jack_counter]
+puts [jack_counter]
+puts [jack_counter]
+puts [jack_counter]
+
+if {[catch {puts [jack_samplerate]} result]} {puts stderr $result}
+jack_register
+puts [jack_samplerate]
+if {[catch {puts [jack_register]} result]} {puts stderr $result}
+jack_deregister
+if {[catch {puts [jack_samplerate]} result]} {puts stderr $result}
+if {[catch {puts [jack_deregister]} result]} {puts stderr $result}
+
+exit
+
