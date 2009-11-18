e459010a3b66ea1c2b154c6c72fba7639c492b0d
Chris Edwards <cedwards@sbis4079.otago.ac.nz>
2009-11-19 01:18:38 +1300

    Corrected minimum sample variable initialisation (trough measurement was previously broken: always read 0). Bumped precision on floating-point values from the meter function, and added DC offset reporting.

--- tcljack.c
+++ tcljack.c
@@ -32,10 +32,10 @@
 
 // Hmm, we're gonna need some global variables for holding server connection state, huh?
 static int counter = 0;
-static jack_client_t *client;
+static jack_client_t *client;	// That's us!
 static jack_status_t status;
 static jack_options_t options = JackNoStartServer;
-static const char *client_name = "tcljack";	// The JACK name for this client.  Could be auto-modified by JACK during registering to avoid dups.
+static const char *client_name = "tcljack";	// The JACK name for this client.  Could be auto-modified by JACK during registering to avoid dups.  "tcljack" or "TclJACK"?  Most other JACK clients use all lowercase.
 static char *server_name = NULL;	// NULL -> not picky.
 // TODO: maybe also a registered flag, to make it less likely that we'll do something crashworthy like deregistering when we aren't registered.  Of course, we could still get booted without knowing about it...although maybe you can register a callback function with JACK for disconnection and other events.
 // http://jackaudio.org/files/docs/html/group__ClientCallbacks.html
@@ -52,23 +52,50 @@ jack_port_t *input_port;
 static float buffer_peak = 0.0;
 static float buffer_trough = 0.0;
 static float buffer_rms = 0.0;
+static float buffer_dc_offset = 0.0;
+// It might be nice to provide an interface for taking longer-term measurements (i.e. keep accumulating indefinitely (until reset)).
+// What about wraparound?!
+static unsigned long long long_term_frame_count = 0;
+static float long_term_peak = 0.0;
+static float long_term_trough = 0.0;
+static float long_term_dc_offset = 0.0;
+static float long_term_rms_sum_of_squares = 0.0;
+// And similarly for periodic metering (accumulate measurements over n frames):
+// Not as nice as being able to specify a number of milliseconds for the window size, but easier to implement!
+// Oh, what if the JACK buffering scheme changes while measuring?!  Just reset these, I guess.  Callback.
+static unsigned long long periodic_frame_count = 0.0;
+static float periodic_peak = 0.0;
+static float periodic_trough = 0.0;
+static float periodic_dc_offset = 0.0;
+static float periodic_rms_sum_of_squares = 0.0;
 // Perhaps also a convenience boolean to indicate presence of a signal on the input port (or, TODO, ports).
 static bool signal_present = false;
+// We _could_ store the sampling rate here and use the JACK callback to update it, but it's probably not worth bothering.
+// static jack_nframes_t jack_samplerate;
 
 // Program info and usage:
-static const unsigned int tcljack_version_major = 0;
-static const unsigned int tcljack_version_minor = 1;
+//static const unsigned int tcljack_version_major = 0;
+//static const unsigned int tcljack_version_minor = 1;
+static const char* tcljack_version = "0.1";
 
 // + version, usage, 
-static char usage_string[] = "Usage forms:\n"
-	"	jack register\n"
-	"	jack deregister\n"
-	"	jack samplerate\n"
-	"	jack cpuload\n"
-	"	jack meter\n"
+static char usage_string[] = "Usage forms:"
+	"\n	jack register"
+	"\n	jack deregister"
+	"\n	jack samplerate"
+	"\n	jack cpuload"
+	"\n	jack meter"
 //	"	jack info ()\n"
 //	"	jack meter -peak -rms -trough\n"
 ;
+
+// Not sure if we can actually make use of this is interp->result assignment.
+#define NOT_REGISTERED_ERROR_STRING "Not connected to JACK server! Try 'jack register'."
+
+
+// A separate function for checking whether we are currently registered to a JACK server and aborting if not.
+// Actually, since this will have to set up a return value, it might have to be a preprocessor macro.
+#define CHECK_JACK_REGISTRATION_STATUS if (!registered) {interp->result = NOT_REGISTERED_ERROR_STRING; return TCL_ERROR; }
 
 
 // Forward/stub/whatever declarations:
@@ -115,7 +142,7 @@ Tcljack_Register(ClientData cdata, Tcl_Interp *interp, int argc,  CONST char *ar
 	// Check if we got assigned a different client name (and update our idea of it, if so).
 	if (status & JackNameNotUnique)
 	{
-		client_name = jack_get_client_name(client);
+		client_name = jack_get_client_name(client);	// Is this going to work OK with it being a const char*?
 		// Can we use stderr in a Tcl extension?
 	//	fprintf (stderr, "unique name `%s' assigned\n", client_name);
 	}
@@ -133,13 +160,14 @@ Tcljack_Register(ClientData cdata, Tcl_Interp *interp, int argc,  CONST char *ar
 		return TCL_ERROR; 
 	}
 
+	// Don't forget to activate this client:
 	if (jack_activate(client)) {
 		interp->result = "JACK: Unable to activate client!";
 		return TCL_ERROR; 
 	}
 
-	// Maybe this should return "OK", or the name of the server, or the name of the client, or something.
-	//
+	// Maybe this should return "1", "OK", the name of the server or client, or something.  Then again, perhaps nothing is fine.
+	//interp->result = "1";
 	return TCL_OK;
 }
 
@@ -167,10 +195,7 @@ Tcljack_Samplerate(ClientData cdata, Tcl_Interp *interp, int argc,  CONST char *
 	char output_buffer[6];	// I doubt that sampling rates in the MHz will be very useful for audio.
 
 	// Don't try to do anything if we're not registered (AFAWCT) (to avoid crash):
-	if (!registered) {
-		interp->result = "Not connected to JACK server!";
-		return TCL_ERROR; 
-	}
+	CHECK_JACK_REGISTRATION_STATUS;
 
 	// Find and return sampling rate:
 	sampling_rate = jack_get_sample_rate(client);
@@ -182,69 +207,70 @@ Tcljack_Samplerate(ClientData cdata, Tcl_Interp *interp, int argc,  CONST char *
 
 // Retrieve the server's current CPU DSP load:
 // TODO: switch to Tcl object interface?
+// This should probably pass a value with as much precision as received; rounding can be done further up if required.
 static int
 Tcljack_Cpuload(ClientData cdata, Tcl_Interp *interp, int argc,  CONST char *argv[])
 {
 	float cpu_load = 0.0;
-	char output_buffer[6];	// How big depends on the format in sprintf below, I guess.
+	char output_buffer[14];	// How big depends on the format in sprintf below, I guess.
 
-	// Don't try to do anything if we're not registered (AFAWCT) (to avoid crash):
-	if (!registered) {
-		interp->result = "Not connected to JACK server!";
-		return TCL_ERROR; 
-	}
+	CHECK_JACK_REGISTRATION_STATUS;
 
 	// Find and return sampling rate:
 	cpu_load = jack_cpu_load(client);	// NOTE: this has already been multiplied by 100%!
-	sprintf(output_buffer, "%0.2f", cpu_load);	// Pad with spaces to the left?  1 DP?  Is the returned value in % or not?
+	sprintf(output_buffer, "%3.10f", cpu_load);	// Pad with spaces to the left?  1 DP?  Is the returned value in % or not?
 	Tcl_SetResult(interp, output_buffer, TCL_VOLATILE);
 
 	return TCL_OK;
 }
 
 
-// CTTOI, we probably want a single command for level metering, to ensure that we use the same measurement window for all measurements.  Returning a tuple, essentially (as a Tcl list, I guess).
-// TODO: handle args for different measurements.  Currently just does peak (testing).  Should support peak, trough, RMS.  Could perhaps optionally do conversion to dB (either numeric or AES-17 dB FS), Stevens RMS loudness, etc. as well.
+// This function provides a single command for level metering, to ensure that we use the same measurement window for all measurements.  Returning a tuple, essentially (as a Tcl list, I guess).
+// TODO: handle args for different measurements.  Peak, trough, and RMS are recorded by process_jack_buffer(); here we just have to output them.
+// TODO: could perhaps optionally do conversion to dB (either numeric or AES-17 dB FS), Stevens RMS loudness, etc. as well.  That stuff isn't happening at the audio data rate, so delegating to the Tcl layer shouldn't be a performance problem.
+// Would be better I think to pass the floats as objects and not have to worry about string formatting here.  TODO: investigate.
 static int
+//Tcljack_Meter(ClientData cdata, Tcl_Interp *interp, int objc,  Tcl_Obj * CONST objv[])
 Tcljack_Meter(ClientData cdata, Tcl_Interp *interp, int argc,  CONST char *argv[])
 {
-	char output_buffer[10];	// How big depends on the format in sprintf below, I guess.
+	Tcl_Obj *result_pointer;
+	char output_buffer[(1 + 3 + 4 * (1 + 1 + 16)) + 1];	// How big depends on the format in sprintf below: 1 for terminating null, 2 spaces, 3 numbers, 1 digit, 1 for decimal point, 8 DP.  Should maybe #define some of these and use in the format below (TODO).
+	// Tcl uses 16 DP for floats, FWIW.
+	// Oh, DC offset could be negative!  Need to reserve space for that possibility.
 
-	// Don't try to do anything if we're not registered (AFAWCT) (to avoid crash):
-	if (!registered) {
-		interp->result = "Not connected to JACK server!";
-		return TCL_ERROR; 
-	}
+	CHECK_JACK_REGISTRATION_STATUS;
 
-	sprintf(output_buffer, "%1.8f", buffer_rms);
+	sprintf(output_buffer, "%1.16f %1.16f %1.16f %1.16f", buffer_peak, buffer_rms, buffer_trough, buffer_dc_offset);
 	Tcl_SetResult(interp, output_buffer, TCL_VOLATILE);
+
+//	Tcl_SetObjResult(interp, Tcl_???());
 
 	return TCL_OK;
 }
 
 
+// DEPRECATED.  New design uses [jack meter -peak].  See Tcljack_Meter.
 // Return the last recorded per-buffer peak sample value (as a raw numeric float, not dB or anything).
 // This will be read asynchronously, but as meters are only going to be refreshing up to say 60 Hz, that's probably fine.  1 / 60 Hz = ~17 ms.  At p=64 Fs=96 kHz, each period would last 666 microseconds.
 // How many digits of precision do we need?  With DSP being applied, who knows, but for plain 24-bit audio: 1 / 2 ^ 24 = ~6e-08, so 8 decimal places is probably reasonable.  JACK floating-point data should not normally exceed 1, so 1 digit before the decimal should be fine.
+/*
 static int
 Tcljack_Peak(ClientData cdata, Tcl_Interp *interp, int argc,  CONST char *argv[])
 {
 	char output_buffer[10];	// How big depends on the format in sprintf below, I guess.
 
-	// Don't try to do anything if we're not registered (AFAWCT) (to avoid crash):
-	if (!registered) {
-		interp->result = "Not connected to JACK server!";
-		return TCL_ERROR; 
-	}
+	CHECK_JACK_REGISTRATION_STATUS;
 
 	sprintf(output_buffer, "%1.8f", buffer_peak);
 	Tcl_SetResult(interp, output_buffer, TCL_VOLATILE);
 
 	return TCL_OK;
 }
+*/
 
 
-// For monitoring, we'll need to set up a port (or two, or n) to receive audio, and define a JACK process() callback fuction.
+
+// For monitoring, we'll need to set up a port (or two, or n) to receive audio, and define a JACK process() callback fuction.  We've called it process_jack_buffer.
 // This will have level metering capability, calculating statistics over the current JACK sample buffer, and copying the results into the relevant global variables from which they can be read asynchronously from Tcl.
 int
 process_jack_buffer(jack_nframes_t nframes, void *arg)
@@ -253,9 +279,9 @@ process_jack_buffer(jack_nframes_t nframes, void *arg)
 //	jack_default_audio_sample_t *in_right;
 	unsigned int i;		// For for-loop index.
 	jack_default_audio_sample_t max_sample = 0.0;		// For peak (largest encountered) value
-	jack_default_audio_sample_t min_sample = 0.0;	// For trough (smallest non-zero) value
+	jack_default_audio_sample_t min_sample = 1.0;	// For trough (smallest non-zero) value
 	jack_default_audio_sample_t rms_sum_of_squares = 0.0;	// For RMS level
-	jack_default_audio_sample_t offset_average = 0.0;	// for DC offset measurement
+	jack_default_audio_sample_t offset_sum = 0.0;	// for DC offset measurement
 	
 	in = jack_port_get_buffer (input_port, nframes);
 	for (i = 0; i < nframes; i++)
@@ -270,12 +296,17 @@ process_jack_buffer(jack_nframes_t nframes, void *arg)
 
 		// For RMS:
 		rms_sum_of_squares += pow(current_sample,2.0);
+
+		// For DC offset (NOTE: don't use current_sample, which is an absolute value!):
+		offset_sum += in[i];
 	}
 
-	// Copy ultimate peak value to global variable:
-	// Or maybe just use buffer_peak in the loop above, why not?
+	// Copy the resulting values to their respective global variables, from which they can be read from the Tcl side.
+	// (Or maybe just use buffer_peak in the loop above, why not?  Ah, but can't do that trick with the RMS value.)
 	buffer_peak = max_sample;
+	buffer_trough = min_sample;
 	buffer_rms = sqrt(rms_sum_of_squares / nframes);
+	buffer_dc_offset = offset_sum / nframes;	// Integer division? Do not want!
 
 	return 0;
 }
@@ -316,19 +347,21 @@ int DLLEXPORT
 Tcljack_Init(Tcl_Interp *interp)
 {
 //	if (Tcl_InitStubs(interp, TCL_VERSION, 0) == NULL)
-	if (Tcl_InitStubs(interp, "8.4", 0) == NULL)	// Just to make it work with expect, which is currently 8.4 on my system, even though it's otherwise Tcl 8.5.
+	if (Tcl_InitStubs(interp, "8.4", 0) == NULL)	// Just to make it work on my test system, which has a messy Tcl 8.4 + 8.5 setup.
 	{
 		return TCL_ERROR;
 	}
 
-	if (Tcl_PkgProvide(interp, "TclJACK", "0.1") == TCL_ERROR)
+	if (Tcl_PkgProvide(interp, "TclJACK", tcljack_version) == TCL_ERROR)
 	{
 		return TCL_ERROR;
 	}
 
 	// Good to go...
+	
 	// Main command is "jack", with subcommands identified and handled by Tcljack_Dispatcher():
 	Tcl_CreateCommand(interp, "jack", Tcljack_Dispatcher, (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
+
 	// The following will all be deprecated by having subcommands, handled by Tcljack_Dispatcher():
 	Tcl_CreateObjCommand(interp, "jack_counter", Tcljack_Counter, NULL, NULL);
 	Tcl_CreateCommand(interp, "jack_register", Tcljack_Register, (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
@@ -336,11 +369,9 @@ Tcljack_Init(Tcl_Interp *interp)
 	Tcl_CreateCommand(interp, "jack_samplerate", Tcljack_Samplerate, (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
 	Tcl_CreateCommand(interp, "jack_cpuload", Tcljack_Cpuload, (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
 
-	Tcl_CreateCommand(interp, "jack_peak", Tcljack_Peak, (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
+//	Tcl_CreateCommand(interp, "jack_peak", Tcljack_Peak, (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
 	Tcl_CreateCommand(interp, "jack_meter", Tcljack_Meter, (ClientData)NULL, (Tcl_CmdDeleteProc *)NULL);
-
-	// How do we implement subcommands, like [jack register] or [jack info]?  Do we just define a main "jack" command and have it figure out whatever subcommand might be being called from the arguments?
-	 
+ 
 	return TCL_OK;
 }
 
