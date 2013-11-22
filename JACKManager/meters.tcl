9f040128ffe26bf114e786ce7f060c258b37133d
Chris Edwards <cedwards.github@cedwards.geek.nz>
2013-11-23 00:18:58 +1300

    Finally, dual sound level meters in JACKManager!

--- JACKManager/meters.tcl
+++ JACKManager/meters.tcl
@@ -41,52 +41,60 @@ proc set_meters_visibility {enabled} {
 	}
 }
 
-proc create_meters {} {
-#	global meter_width meter_height
-	# There's just one meter at the moment.
-	namespace eval ::meters::meter0 {}
-	namespace eval ::meters::meter1 {}
 
-	# Probably want a frame around the whole meter set, as we don't know how many we'll have.
-	frame .meters -borderwidth 1
+proc update_meter {meter_num} {
+	global meter_width meter_height meter_clipping_point
 
-	grid [frame .meters.sound_gauge  -width $::meters::meter_width  -height $::meters::meter_height  -relief sunken  -borderwidth 1 -background black] -row 0 -column 0
+	# How expensive is all this stuff...?
 
-	# Meter gauge is also simply done as a frame:
-	place [frame .meters.sound_gauge.meter     -width [expr {$::meters::meter_width-2}] -height 0  -relief flat -borderwidth 0 -background green] -anchor sw -x 0 -y [expr {$::meters::meter_height-2}]
+	# Peak level is useful for clipping indicator; RMS for overall level.
+	set meter_reading [lindex [jack meter] $meter_num]
+	set raw_peak_level [lindex $meter_reading 0]
+	set raw_rms_level [lindex $meter_reading 1]
+	set raw_trough_level [lindex $meter_reading 2]
+	set raw_dc_offset [lindex $meter_reading 3]
 
-	setTooltip .meters {Audio signal level(s)}
+	set gauge_colour green	;# Default, everything-is-OK gauge colour.
+	# Cheating to get the meter to appear invisible with silence:
+	if {$raw_peak_level == 0} then {set gauge_colour black}	;# Ha!
+	# For colouring the meter, first check the RMS level and if it's above -14 dB FS (-17 dB numeric), colour the gauge orange or yellow.
+	if {$raw_rms_level > 0.14} then {set gauge_colour yellow}
+	# Or could we base it on peak level?  0.5 or 0.3 threshold?
+#	if {$raw_peak_level > 0.3} then {set gauge_colour yellow}
+	# Then, check the peak level for clipping, and colour it red if clipping occurred.
+	if {$raw_peak_level > $::meters::meter_clipping_threshold} then {set gauge_colour red}
 
-	# Start the meter updating in the background:
-	# TODO: properly figure out how often to update.  Every 16 ms might be reasonable assuming a 60 Hz display refresh rate.  Or, we could try to update when the JACK process() happens...might be more often than necessary?
-	set ::meters::meter0::updater [every 50 {
-		global meter_width meter_height meter_clipping_point
-
-		# How expensive is all this stuff...?
-
-		# Peak level is useful for clipping indicator; RMS for overall level.
-		set meter_reading [jack meter]
-		set raw_peak_level [lindex $meter_reading 0]
-		set raw_rms_level [lindex $meter_reading 1]
-		set raw_trough_level [lindex $meter_reading 2]
-		set raw_dc_offset [lindex $meter_reading 3]
-
-		set gauge_colour green	;# Default, everything-is-OK gauge colour.
-		# Cheating to get the meter to appear invisible with silence:
-		if {$raw_peak_level == 0} then {set gauge_colour black}	;# Ha!
-		# For colouring the meter, first check the RMS level and if it's above -14 dB FS (-17 dB numeric), colour the gauge orange or yellow.
-		if {$raw_rms_level > 0.14} then {set gauge_colour yellow}
-		# Or could we base it on peak level?  0.5 or 0.3 threshold?
-	#	if {$raw_peak_level > 0.3} then {set gauge_colour yellow}
-		# Then, check the peak level for clipping, and colour it red if clipping occurred.
-		if {$raw_peak_level > $::meters::meter_clipping_threshold} then {set gauge_colour red}
-
-		set gauge_value [clip [expr {2.5 * pow($raw_rms_level, 0.67)}] 0 1]
-		.meters.sound_gauge.meter configure -height [expr {$gauge_value * ($::meters::meter_height-2)}] -background $gauge_colour
-	}]
+	set gauge_value [clip [expr {2.5 * pow($raw_rms_level, 0.67)}] 0 1]
+	.meters.meter${meter_num}.meter configure -height [expr {$gauge_value * ($::meters::meter_height-2)}] -background $gauge_colour
 }
 
-proc destroy_meters {} {destroy .meters; every cancel $::meters::meter0::updater}
+
+proc create_meters {} {
+	# Probably want a frame around the whole meter set, as we don't know how many we'll have.
+	frame .meters -borderwidth 1
+	setTooltip .meters {Audio signal level}
+
+	# Create meters.  Probably at least two, for stereo use...
+	foreach meter_num {0 1} {
+
+		grid [frame .meters.meter${meter_num}  -width $::meters::meter_width  -height $::meters::meter_height  -relief sunken  -borderwidth 1 -background black] -row 0 -column $meter_num
+
+		# Meter gauge is also simply done as a frame:
+		place [frame .meters.meter${meter_num}.meter     -width [expr {$::meters::meter_width-2}] -height 0  -relief flat -borderwidth 0 -background green] -anchor sw -x 0 -y [expr {$::meters::meter_height-2}]
+
+		# Start the meter updating in the background:
+		# TODO: properly figure out how often to update.  Every 16 ms might be reasonable assuming a 60 Hz display refresh rate.  Or, we could try to update when the JACK process() happens...might be more often than necessary?
+		# TODO: have a single "every" to update all the meters!  For both efficiency and to have the meters updated from the same analysis slice (if it's possible to ensure that...?!).  Where to store the meter IDs?
+		set ::meters::updater($meter_num) [every 50 [list update_meter $meter_num]]
+	}
+}
+
+proc destroy_meters {} {
+	destroy .meters
+	foreach meter_num {0 1} {
+		every cancel $::meters::updater($meter_num)
+	}
+}
 
 proc show_meters {} {grid .meters -row 0 -column 5}
 
